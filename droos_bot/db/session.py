68031fd83b90c34543074edbabbd1d0667fe5bf0
Mohammad1Ahmed1ALhayek <mohamm512008@gmail.com>
2026-09-16 14:16:38 +0300

    Fix SQLite path for Render

--- droos_bot/db/session.py
+++ droos_bot/db/session.py
@@ -1,12 +1,14 @@
 """Database initialization."""
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from droos_bot import PARENT_DIR
 from droos_bot.db.base import Base
 
-db_connection_string = f"sqlite:///{PARENT_DIR}/droos_bot.db"
+db_path = "/tmp/droos_bot.db" if os.environ.get("RENDER") else str(PARENT_DIR / "droos_bot.db")
+db_connection_string = f"sqlite:///{db_path}"
 engine = create_engine(db_connection_string, connect_args={"check_same_thread": False})
 
 Base.metadata.create_all(bind=engine)
